30f3afc35c0b218ff8a1dc61f767a6133f2a9a79
Marwan_Nabil <141582334+marwan-info@users.noreply.github.com>
2025-05-03 15:58:03 +0300

    Update part1.m

--- part1.m
+++ part1.m
@@ -6,9 +6,9 @@ dt = 1 / Fs;
 t = -10*T:dt:10*T;              % Time vector, wide enough to contain full pulse
 N = length(t);
 
-%% message Square Pulse (duration T, centered at t = 0)  
-width = 2*T ;
-square_pulse = rectpuls(t, width); % from [40us to -40 us] in freq turn to sinc with nulls at 25 , 50 , 100 , .......
+%% message Square Pulse (duration T, centered at t = 10us)  
+width = T ;
+square_pulse = rectpuls(t-(T/2), width); % from [0us to 20 us] in freq turn to sinc with nulls at 25 , 50 , 100 , .......
 %% Frequency vector for plotting
 f = linspace(-Fs/2, Fs/2, N);   % Frequency axis centered at 0 Hz
 
@@ -22,7 +22,7 @@ thefilter = rectpuls(f,2*B);           % 1 in [-B, B], 0 elsewhere
 %% Plot Both in Frequency Domain
 figure;
 plot(f/1e3, X_mag, 'b', 'LineWidth', 2); hold on;
-plot(f/1e3, thefilter, 'r--', 'LineWidth', 2);
+plot(f/1e3, thefilter, 'r', 'LineWidth', 2);
 xlabel('Frequency (kHz)');
 ylabel('Magnitude (normalized)');
 title('Frequency Domain: Square Pulse Spectrum vs Ideal Filter');
@@ -40,7 +40,7 @@ plot(t*1e6, square_pulse, 'b', 'LineWidth', 1.5); hold on;
 plot(t*1e6, filtered_time, 'r', 'LineWidth', 1.5);
 xlabel('Time (\mus)'); ylabel('Amplitude');
 legend('Original', 'Filtered');
-title('Square Pulse Before and After Band-Limited Channel ');
+title('time domain : Square Pulse Before and After Band-Limited Channel ');
 
 grid on;
 
@@ -54,25 +54,25 @@ title('Frequency Domain Before and After Filtering');
 xlim([-200 200]);
 grid on;
 
-pulse1 = rectpuls((t - T) ,T/2);  % First pulse at t = T
-pulse2 = rectpuls((t - 3*T) , T/2); % Second pulse at t = 3T
+pulse1 = rectpuls((t - T/2)- T ,width);  % First pulse at t = T 20us
+pulse2 = rectpuls((t - T/2)- 3*T , width); % Second pulse at t = 3T 60us
 
 
 % Frequency-domain filtering
-P1_F = fftshift(fft(pulse1));
-P1_Filt = P1_F .* thefilter;
+Pulse1_F = fftshift(fft(pulse1));
+P1_Filt = Pulse1_F .* thefilter;
 pulse1_out = ifft(ifftshift(P1_Filt), 'symmetric');
 
-P2_F = fftshift(fft(pulse2));
-P2_Filt = P2_F .* thefilter;
+Pulse2_F = fftshift(fft(pulse2));
+P2_Filt = Pulse2_F .* thefilter;
 pulse2_out = ifft(ifftshift(P2_Filt), 'symmetric');
 
 figure;
-plot(t*1e6, pulse1, 'b--', 'LineWidth', 1.2); hold on;
-plot(t*1e6, pulse2, 'g--', 'LineWidth', 1.2);
+plot(t*1e6, pulse1, 'b', 'LineWidth', 1.2); hold on;
+plot(t*1e6, pulse2, 'g', 'LineWidth', 1.2);
 xlabel('Time (\mus)'); ylabel('Amplitude');
 legend('Pulse 1 (input)', 'Pulse 2 (input)');
-title('Original Two Square Pulses Before Channel');
+title('time domain : Original Two Square Pulses Before Channel');
 grid on;
 
 figure;
@@ -80,5 +80,5 @@ plot(t*1e6, pulse1_out, 'b', 'LineWidth', 1.5); hold on;
 plot(t*1e6, pulse2_out, 'g', 'LineWidth', 1.5);
 xlabel('Time (\mus)'); ylabel('Amplitude');
 legend('Pulse 1 (output)', 'Pulse 2 (output)');
-title('3-Filtered Pulses After Band-Limited Channel');
+title('time domain : Filtered Pulses After Band-Limited Channel');
 grid on;
